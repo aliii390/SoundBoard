0f3c2b8e5f26221ccd1a7651ce894d2f6403284d
aliii390 <alli.said.errahmani@gmail.com>
2024-11-25 16:47:34 +0100

    ADD: fin de btn record il manque la partie play

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
+// document.querySelectorAll('.key').forEach((key) => {
+//         key.add
+//     //  })
+
+let recording = false;
+let playing = false;
+
+let notesEnregistrer = [];
+
 window.addEventListener("keydown", handleKeyDownAudio);
+window.addEventListener("keyup", handleKeyup);
+window.addEventListener("keydown", handleKeyDownRecord);
+// window.addEventListener("keydown", handleKeyDownPlay);
 
-function handleKeyDownAudio(event){
-    if(event.repeat){return}
-    // console.log(event.keyCode);
-    var audio =document.querySelector(`audio[data-key="${event.keyCode}"]`).src;
-    // console.log(audio);
-    audio = new Audio(audio)
+function handleKeyDownAudio(event) {
+  if (event.repeat) {
+    return;
+  }
+  // console.log(event.keyCode);
+  if (event.keyCode !== 82 && event.keyCode !== 80) {
+    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    audio.currentTime = 0;
+    audio.play();
 
-         audio.play();
-    var key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    let key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     key.classList.add("playing");
-    setTimeout(() => { key.classList.remove("playing"); }, 100);
-    key.classList.add("sound");
-    setTimeout(() => { key.classList.remove("sound"); }, 100);
 
+    if (recording === true) {
+      notesEnregistrer.push(event.keyCode);
+      console.log(notesEnregistrer);
     }
+  }
+}
 
 
 
+function handleKeyup(event) {
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (!key) return;
 
+  if (event.keyCode !== 82) {
+    key.classList.remove("playing");
+  }
+}
 
+let count = 0;
 
+function handleKeyDownRecord(event) {
+  if (event.keyCode === 82) {
+    let key = document.querySelector(`.key[data-key="82"]`);
+    key.classList.toggle("playing");
+    recording = !recording;
+    notesEnregistrer = []
+  }
+}
 
-// cherche solution pour record 
-
-
-    window.addEventListener("keydown", handleRecord);
-
-
-
-    function handleRecord(event){
-        audio
-    }
-
-
-
-
+// function handleKeyDownPlay(event) {
+//   if (event.keyCode == "80") {
+//     let key = document.querySelector(`.key[data-key="80"]`);
+//     key.classList.toggle("playing");
+//     setTimeout(() => {
+//       key.classList.remove("playing");
+//     }, 200);
+//   }
+// }
